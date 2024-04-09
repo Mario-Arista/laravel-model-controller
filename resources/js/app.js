e71daf2042707dc599262499b4cb817d6fa0c3ed
Mario Arista <149085849+Mario-Arista@users.noreply.github.com>
2024-04-09 13:45:24 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,12 @@
+import './bootstrap';
+
+// Import our custom CSS
+import '~resources/scss/app.scss';
+
+/* Import Bootstrap 5 */
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+import.meta.glob([
+    '../img/**'
+]);
